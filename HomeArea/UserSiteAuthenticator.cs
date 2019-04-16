219a3b5fa7acdd613368da3f4baca7a423c1a5d3
yasharvc@yahoo.com <yasharvc@yahoo.com>
2019-04-16 14:32:00 +0430

    Generalizing permission checking and handling with PermissionProvider

---
+++ HomeArea/UserSiteAuthenticator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using ModuloContracts.MVC;
+using ModuloContracts.Web;
+using System;
+
+namespace HomeArea
+{
+	public class UserSiteAuthenticator : PermissionProvider
+	{
+		private static readonly string HomeAreaUserToken = "_HOME_USER_TOKEN_";
+		private string GetToken(HttpContext context) => context.Request.Cookies[HomeAreaUserToken];
+		public override void Authenticate(HttpContext context, string token) => context.Response.Cookies.Append(HomeAreaUserToken, token);
+
+		public override void Disprove(HttpContext context) => context.Response.Cookies.Delete(HomeAreaUserToken);
+
+		public override bool IsAuthenticated(HttpContext context) => GetToken(context) != null;
+
+		public override bool IsInScope(PathParts path) => !path.ModuleName.Equals("moduleadmin", StringComparison.OrdinalIgnoreCase);
+	}
+}
---
+++ ModuloContracts/MVC/PermissionProvider.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using ModuloContracts.Web;
+
+namespace ModuloContracts.MVC
+{
+	public abstract class PermissionProvider
+	{
+		public abstract bool IsInScope(PathParts path);
+		public abstract bool IsAuthenticated(HttpContext context);
+		public abstract void Authenticate(HttpContext context,string token);
+		public abstract void Disprove(HttpContext context);
+	}
+}
